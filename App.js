430f3bdbaae53a21ba1a2ef2235cec2848cdc0e2
Cleibel30 <cleibel30052006@gmail.com>
2025-12-28 22:50:44 -0400

    exporoot configuracion

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { ExpoRoot } from "expo-router";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Chat app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
